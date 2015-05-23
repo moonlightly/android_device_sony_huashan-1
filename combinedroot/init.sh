a3e2583ef709d327be8434cd58d6fadd0295b652
Adrian DC <AdrianDC@users.noreply.github.com>
2015-05-23 16:18:41 +0200

    Boot LEDs animation and colors
    Adapted to work properly with offline charging LEDs and recovery.

--- combinedroot/init.sh
+++ combinedroot/init.sh
@@ -4,11 +4,23 @@ _PATH="$PATH"
 export PATH=/sbin
 
 LED_RED="/sys/class/leds/LED1_R/brightness"
-LED_RED_CURRENT="/sys/class/leds/LED1_R/led_current"
+LED_GREEN="/sys/class/leds/LED1_G/brightness"
+LED_BLUE="/sys/class/leds/LED1_B/brightness"
 LED2_RED="/sys/class/leds/LED2_R/brightness"
-LED2_RED_CURRENT="/sys/class/leds/LED2_R/led_current"
+LED2_GREEN="/sys/class/leds/LED2_G/brightness"
+LED2_BLUE="/sys/class/leds/LED2_B/brightness"
 LED3_RED="/sys/class/leds/LED3_R/brightness"
+LED3_GREEN="/sys/class/leds/LED3_G/brightness"
+LED3_BLUE="/sys/class/leds/LED3_B/brightness"
+LED_RED_CURRENT="/sys/class/leds/LED1_R/led_current"
+LED_GREEN_CURRENT="/sys/class/leds/LED1_G/led_current"
+LED_BLUE_CURRENT="/sys/class/leds/LED1_B/led_current"
+LED2_RED_CURRENT="/sys/class/leds/LED2_R/led_current"
+LED2_GREEN_CURRENT="/sys/class/leds/LED2_G/led_current"
+LED2_BLUE_CURRENT="/sys/class/leds/LED2_B/led_current"
 LED3_RED_CURRENT="/sys/class/leds/LED3_R/led_current"
+LED3_GREEN_CURRENT="/sys/class/leds/LED3_G/led_current"
+LED3_BLUE_CURRENT="/sys/class/leds/LED3_B/led_current"
 
 busybox cd /
 busybox date >>boot.txt
@@ -33,16 +45,47 @@ busybox mknod -m 666 /dev/null c 1 3
 busybox mount -t proc proc /proc
 busybox mount -t sysfs sysfs /sys
 
-echo '96' > $LED_RED
-echo '96' > $LED_RED_CURRENT
-echo '96' > $LED3_RED
-echo '96' > $LED3_RED_CURRENT
-echo '96' > $LED2_RED
-echo '96' > $LED2_RED_CURRENT
+# lights off
+echo '0' > $LED_RED
+echo '0' > $LED_RED_CURRENT
+echo '0' > $LED_GREEN
+echo '0' > $LED_GREEN_CURRENT
+echo '0' > $LED_BLUE
+echo '0' > $LED_BLUE_CURRENT
+echo '0' > $LED2_RED
+echo '0' > $LED2_RED_CURRENT
+echo '0' > $LED2_GREEN
+echo '0' > $LED2_GREEN_CURRENT
+echo '0' > $LED2_BLUE
+echo '0' > $LED2_BLUE_CURRENT
+echo '0' > $LED3_RED
+echo '0' > $LED3_RED_CURRENT
+echo '0' > $LED3_GREEN
+echo '0' > $LED3_GREEN_CURRENT
+echo '0' > $LED3_BLUE
+echo '0' > $LED3_BLUE_CURRENT
+
+# lights animations
+echo '128' > $LED3_RED
+echo '128' > $LED3_RED_CURRENT
+busybox sleep 1
+echo '128' > $LED_GREEN
+echo '128' > $LED_GREEN_CURRENT
+busybox sleep 1
+echo '128' > $LED2_BLUE
+echo '128' > $LED2_BLUE_CURRENT
 
 # keycheck
 busybox cat ${BOOTREC_EVENT} > /dev/keycheck&
 busybox sleep 3
+
+# lights off
+echo '0' > $LED_GREEN
+echo '0' > $LED_GREEN_CURRENT
+echo '0' > $LED2_BLUE
+echo '0' > $LED2_BLUE_CURRENT
+echo '0' > $LED3_RED
+echo '0' > $LED3_RED_CURRENT
 
 # android ramdisk
 load_image=/sbin/ramdisk.cpio
@@ -66,24 +109,32 @@ if [ -s /dev/keycheck ] || busybox grep -q warmboot=0x77665502 /proc/cmdline ; t
 	load_image=/sbin/ramdisk-recovery.cpio
 else
 	busybox echo 'ANDROID BOOT' >>boot.txt
-	# poweroff LED
-	echo '96' > $LED_RED
-	echo '96' > $LED_RED_CURRENT
-	echo '96' > $LED3_RED
-	echo '96' > $LED3_RED_CURRENT
-	echo '96' > $LED2_RED
-	echo '96' > $LED2_RED_CURRENT
+	echo '128' > $LED_GREEN
+	echo '128' > $LED_GREEN_CURRENT
+	echo '128' > $LED2_GREEN
+	echo '128' > $LED2_GREEN_CURRENT
+	echo '128' > $LED3_GREEN
+	echo '128' > $LED3_GREEN_CURRENT
 fi
 
 # kill the keycheck process
 busybox pkill -f "busybox cat ${BOOTREC_EVENT}"
 busybox echo '0' > /sys/class/timed_output/vibrator/enable
-echo '96' > $LED_RED
-echo '96' > $LED_RED_CURRENT
-echo '96' > $LED3_RED
-echo '96' > $LED3_RED_CURRENT
-echo '96' > $LED2_RED
-echo '96' > $LED2_RED_CURRENT
+
+# lights off
+busybox sleep 1
+echo '0' > $LED_RED
+echo '0' > $LED_RED_CURRENT
+echo '0' > $LED3_RED
+echo '0' > $LED3_RED_CURRENT
+echo '0' > $LED2_RED
+echo '0' > $LED2_RED_CURRENT
+echo '0' > $LED_GREEN
+echo '0' > $LED_GREEN_CURRENT
+echo '0' > $LED2_GREEN
+echo '0' > $LED2_GREEN_CURRENT
+echo '0' > $LED3_GREEN
+echo '0' > $LED3_GREEN_CURRENT
 
 busybox umount /proc
 busybox umount /sys
